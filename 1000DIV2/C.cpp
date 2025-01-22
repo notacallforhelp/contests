0367d717ef9a6243ea7b2e7dfd6d6120d41b0e05
shamirul <hudashamirul@gmail.com>
2025-01-22 21:42:40 +0530

    DIV 2 1000 done

---
+++ 1000DIV2/C.cpp
@@ -0,0 +1,303 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+typedef long long ll;
+typedef long double ld;
+
+//ordered_sets
+template <typename T>
+using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+//struct for range 
+struct range
+{
+    int l,r,index;
+    bool operator < (const range &other) const
+    {
+        if(l==other.l)
+            return r>other.r;
+        return l < other.l;
+    }
+};
+
+/*
+FOR SIMULATING ALL CELLS THAT SHARE A WALL WITH CURRENT CELL, GRID IS OF SIZE N*M
+
+int dx[]={-1,0,+1,0};
+int dy[]={0,-1,0,+1};
+
+inline bool in(int i,int j){
+    return (0<=i&&i<n&&0<=j&&j<m);
+}
+
+
+binary exp 
+
+ll binpow(ll a,ll b)
+{
+    if(b==0)
+    {
+        return 1;
+    }
+    if(b%2)
+    {
+        return (a*binpow(a,b-1))%mod;
+    }
+    return binpow((a*a)%mod,b/2);
+}
+
+ceil 
+
+ll ceil2(ll a, ll b) {
+    if (a == 0) return 0;
+    return (a - 1)/b + 1;
+}
+
+COMBINATORICS TEMPLATE 
+
+const int N = 2e5 + 5, mod = 1e9 + 7;
+int64_t fact[N];
+int64_t pw(int64_t a, int64_t b) {
+	int64_t r = 1;
+	while(b > 0) {
+		if(b & 1) r = (r * a) % mod;
+		b /= 2;
+		a = (a * a) % mod; 
+	}
+	return r;
+}
+int64_t C(int64_t n, int64_t k) {
+	if(n < k) return 0LL;
+	return (fact[n] * pw((fact[n - k] * fact[k]) % mod, mod - 2)) % mod;
+}
+
+void find_divisors()
+{
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=i;j<=N;j+=i)
+        {
+            divisors[j].push_back(i);
+        }
+    }
+}
+
+DFS
+
+vector<vector<int>> adj(n);
+vector<bool> visited(n);
+
+void dfs(int current_node) {
+	if (visited[current_node]) { return; }
+	visited[current_node] = true;
+
+	for (int neighbor : adj[current_node]) { dfs(neighbor); }
+}
+
+SIEVE
+
+void sieve(int n,vector<bool> &prime)
+{
+    for(int i=2;i*i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int p=i*i;p<=n;p+=i)
+            {
+                prime[p]=false;
+            }
+        }
+    }
+}
+prime[0]=prime[1]=false;
+
+*/
+
+/*void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}*/
+
+void dfs(int vertex,vector<vector<int>> &graph,vector<int> &height,int par=0)
+{
+    //action on vertex after enter
+
+    for(int child: graph[vertex])
+    {
+        //action on child before enter
+
+        if(child==par) continue;
+        dfs(child,graph,height,vertex);
+        height[vertex]=max(height[vertex],height[child]+1);
+        //action on child after exit
+
+    }
+    //action on vertex before exit
+}
+
+void solve()
+{
+    int n; cin>>n;
+
+    vector<vector<int>> graph(n+1);
+
+    vector<vector<int>> G(n+1);
+
+    vector<int> height(n+1);
+
+    for(int i=0;i<n-1;i++)
+    {
+        int u,v; cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+        G[u].push_back(v);
+    }
+
+    /*if(n==2)
+    {
+        cout << 0 << endl;
+        return;
+    }*/
+
+    dfs(1,graph,height,0);
+
+    /*for(int i=1;i<=n;i++)
+    {
+        cout << height[i] << " ";
+    }
+    cout << endl;*/
+
+    map<int,int> M;  // stores number of children with height 0 for given vertex
+
+    for(int i=1;i<=n;i++)
+    {
+        for(auto &ele:graph[i])
+        {
+            if(height[ele]==0)
+            {
+                ++M[i];
+            }
+        }
+    }
+
+    /*pair<int,int> mx ={0,-1};
+
+    for(int i=1;i<=n;i++)
+    {
+        //cout << i << " " << M[i] << endl;
+        if(M[i]>mx.second)
+        {
+            mx = {i,M[i]};
+        }
+    }*/
+
+   /*for(int i=1;i<=n;i++)
+   {
+    cout << M[i] << " ";
+   }
+   cout << endl;*/
+
+    vector<pair<int,int>> prs;
+
+    for(int i=1;i<=n;i++)
+    {
+            prs.push_back({M[i],i});
+    }
+
+    sort(prs.begin(),prs.end());
+
+    /*for(int i=prs.size()-1;i>=0;i--)
+    {
+        cout << prs[i].first << " " <<prs[i].second << endl;
+    }*/
+
+    vector<pair<int,int>> k;
+
+    for(int i=prs.size()-2;i<prs.size();i++)
+    {
+        k.push_back({prs[i].second,prs[i].first});
+    }
+
+    sort(k.begin(),k.end());
+
+    /*for(auto &ele:k)
+    {
+        cout << ele.first << " " << ele.second << endl;
+    }*/
+
+    bool cut = false;
+
+    int output = 0;
+
+    //cout << height[4] << endl;
+
+    //cout << G[2].size() << endl;
+
+    for(int i=0;i<k.size();i++)
+    {
+        int vert = k[i].first;
+        if(height[vert]==0)
+        {
+            --output;
+            continue;
+        }
+        if(vert==1)
+        {
+            cut = true;
+            if(G[vert].size()!=M[vert])
+            {
+                output += M[vert]+1;
+            }
+            else
+            {
+                output += M[vert];
+            }
+            //cout << output << endl;
+        }
+        else
+        {
+            if(G[vert].size()!=M[vert])
+            {
+                output += M[vert]+1;
+            }
+            else
+            {
+                output += M[vert];
+            }
+            if(!cut)
+            {
+                ++output;
+                cut = true;
+            }
+        }
+        /*if(i==0)
+        {
+            cout << output << endl;
+        }*/
+    }
+
+    cout << output << endl;
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.precision(20);
+
+    //setIO("problemname");
+
+    int t; cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
