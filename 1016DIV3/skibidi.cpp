5f988bda5f9690d25b9992ec09298263136339ce
shamirul <hudashamirul@gmail.com>
2025-04-10 20:28:35 +0530

    upsolved

---
+++ 1016DIV3/skibidi.cpp
@@ -0,0 +1,298 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+typedef long long ll;
+typedef long double ld;
+
+//ordered_sets
+template <typename T>
+using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+//struct for range 
+struct range
+{
+    int l,r,index;
+    bool operator < (const range &other) const
+    {
+        if(l==other.l)
+            return r>other.r;
+        return l < other.l;
+    }
+};
+
+/*
+FOR SIMULATING ALL CELLS THAT SHARE A WALL WITH CURRENT CELL, GRID IS OF SIZE N*M
+
+int dx[]={-1,0,+1,0};
+int dy[]={0,-1,0,+1};
+
+inline bool in(int i,int j){
+    return (0<=i&&i<n&&0<=j&&j<m);
+}
+
+
+binary exp 
+
+ll binpow(ll a,ll b)
+{
+    if(b==0)
+    {
+        return 1;
+    }
+    if(b%2)
+    {
+        return (a*binpow(a,b-1))%mod;
+    }
+    return binpow((a*a)%mod,b/2);
+}
+
+ceil 
+
+ll ceil2(ll a, ll b) {
+    if (a == 0) return 0;
+    return (a - 1)/b + 1;
+}
+
+COMBINATORICS TEMPLATE 
+
+const int N = 2e5 + 5, mod = 1e9 + 7;
+int64_t fact[N];
+int64_t pw(int64_t a, int64_t b) {
+	int64_t r = 1;
+	while(b > 0) {
+		if(b & 1) r = (r * a) % mod;
+		b /= 2;
+		a = (a * a) % mod; 
+	}
+	return r;
+}
+int64_t C(int64_t n, int64_t k) {
+	if(n < k) return 0LL;
+	return (fact[n] * pw((fact[n - k] * fact[k]) % mod, mod - 2)) % mod;
+    // return fact[n]*%mod*ifact[n-k]%mod*ifact[k]%mod;
+}
+
+ifact[N-1] = pw(fact[N-1],mod-2);
+for(int i=N-2;i>=0;i--)
+{
+    ifact[i]=1ll*ifact[i+1]*(i+1)%mod;
+}
+
+void find_divisors()
+{
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=i;j<=N;j+=i)
+        {
+            divisors[j].push_back(i);
+        }
+    }
+}
+
+DFS
+
+vector<vector<int>> adj(n);
+vector<bool> visited(n);
+
+void dfs(int current_node) {
+	if (visited[current_node]) { return; }
+	visited[current_node] = true;
+
+	for (int neighbor : adj[current_node]) { dfs(neighbor); }
+}
+
+BFS
+
+for (int i = 0; i < n; i++) {
+		// iterate over all connected components in the graph
+		if (!visited[i]) {
+			queue<int> q;
+			q.push(i);
+			visited[i] = true;
+			while (!q.empty()) {
+				int current_node = q.front();
+				q.pop();
+				for (int neighbor : adj[current_node]) {
+					if (!visited[neighbor]) {
+						visited[neighbor] = true;
+						q.push(neighbor);
+					}
+				}
+			}
+		}
+	}
+
+SIEVE
+
+void sieve(int n,vector<bool> &prime)
+{
+    for(int i=2;i*i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int p=i*i;p<=n;p+=i)
+            {
+                prime[p]=false;
+            }
+        }
+    }
+}
+prime[0]=prime[1]=false;
+
+*/
+
+/*void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}*/
+
+ll binpow(ll a,ll b)
+{
+    if(b==0)
+    {
+        return 1;
+    }
+    if(b%2)
+    {
+        return (a*binpow(a,b-1));
+    }
+    return binpow((a*a),b/2);
+}
+
+void solve()
+{
+    int n; cin>>n;
+
+    //int mult = binpow(4ll,n-1);
+
+    int q; cin>>q;
+    while(q--)
+    {
+        string s; cin>>s;
+        if(s=="->")
+        {
+            int a,b; cin>>a>>b;
+            int x = 1,y=1;
+            int N = n;
+            int begin = 1;
+            int turns = 0;
+            int jumper = binpow(2ll,N-1);
+
+            while(x!=a||y!=b)
+            {
+                int mult = binpow(4ll,N-1);
+                vector<int> k;
+                for(int j=1;j<=4;j++)
+                {
+                    k.push_back(begin+j*mult-1);
+                }
+                int x_diff = a-x;
+                int y_diff = b-y;
+                int idx;
+
+                if(x+jumper<=a&&y+jumper<=b)
+                {
+                    x+=jumper;
+                    y+=jumper;
+                    idx = 1;
+                }
+                else if(x+jumper<=a&&y+jumper>b)
+                {
+                    x+= jumper;
+                    idx = 2;
+                }
+                else if(y+jumper<=b&&x+jumper>a)
+                {
+                    y+= jumper;
+                    idx = 3;
+                }
+                else
+                { 
+                    idx = 0;
+                }
+                begin = k[idx]-mult+1;
+                --N;
+                jumper /=2;
+                if(N==0)
+                {
+                    break;
+                }
+            }
+            cout << begin << endl;
+        }
+        else
+        {
+            int d; cin>>d;
+            int x = 1,y=1;
+            int N = n;
+            int begin = 1;
+            int turns = 0;
+            int jumper = binpow(2ll,N-1);
+
+            while(begin!=d)
+            {
+                int mult = binpow(4ll,N-1);
+                vector<int> k;
+                for(int j=1;j<=4;j++)
+                {
+                    k.push_back(begin+j*mult-1);
+                }
+                /*for(auto &ele:k)
+                {
+                    //cout << ele << " ";
+                }*/
+                //cout << endl;
+                int idx = lower_bound(k.begin(),k.end(),d)-k.begin();
+                if(idx==0)
+                {
+                    
+                }
+                else if(idx==1)
+                {
+                    x+=jumper;
+                    y+=jumper;
+                }
+                else if(idx==2)
+                {
+                    x+=jumper;
+                }
+                else if(idx==3)
+                {
+                    y+=jumper;
+                }
+                begin = k[idx]-mult+1;
+                //cout << idx << endl;
+                //cout << begin<<endl;
+                --N;
+                jumper /=2;
+                if(N==0)
+                {
+                    break;
+                }
+            }
+            cout << x << " " << y << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.precision(20);
+
+    //setIO("problemname");
+
+    int t; cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
