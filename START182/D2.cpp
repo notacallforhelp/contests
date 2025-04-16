1372cd4dfac74de370009ad59549f7a3971a3d92
shamirul <hudashamirul@gmail.com>
2025-04-17 00:16:32 +0530

    gave contest

---
+++ START182/D2.cpp
@@ -0,0 +1,380 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+typedef long long ll;
+typedef long double ld;
+
+//ordered_sets
+template <typename T>
+using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+//struct for range 
+struct range
+{
+    int l,r,index;
+    bool operator < (const range &other) const
+    {
+        if(l==other.l)
+            return r>other.r;
+        return l < other.l;
+    }
+};
+
+/*
+FOR SIMULATING ALL CELLS THAT SHARE A WALL WITH CURRENT CELL, GRID IS OF SIZE N*M
+
+int dx[]={-1,0,+1,0};
+int dy[]={0,-1,0,+1};
+
+inline bool in(int i,int j){
+    return (0<=i&&i<n&&0<=j&&j<m);
+}
+
+
+binary exp 
+
+ll binpow(ll a,ll b)
+{
+    if(b==0)
+    {
+        return 1;
+    }
+    if(b%2)
+    {
+        return (a*binpow(a,b-1))%mod;
+    }
+    return binpow((a*a)%mod,b/2);
+}
+
+ceil 
+
+ll ceil2(ll a, ll b) {
+    if (a == 0) return 0;
+    return (a - 1)/b + 1;
+}
+
+COMBINATORICS TEMPLATE 
+
+const int N = 2e5 + 5, mod = 1e9 + 7;
+int64_t fact[N];
+int64_t pw(int64_t a, int64_t b) {
+	int64_t r = 1;
+	while(b > 0) {
+		if(b & 1) r = (r * a) % mod;
+		b /= 2;
+		a = (a * a) % mod; 
+	}
+	return r;
+}
+int64_t C(int64_t n, int64_t k) {
+	if(n < k) return 0LL;
+	return (fact[n] * pw((fact[n - k] * fact[k]) % mod, mod - 2)) % mod;
+    // return fact[n]*%mod*ifact[n-k]%mod*ifact[k]%mod;
+}
+
+ifact[N-1] = pw(fact[N-1],mod-2);
+for(int i=N-2;i>=0;i--)
+{
+    ifact[i]=1ll*ifact[i+1]*(i+1)%mod;
+}
+
+void find_divisors()
+{
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=i;j<=N;j+=i)
+        {
+            divisors[j].push_back(i);
+        }
+    }
+}
+
+DFS
+
+vector<vector<int>> adj(n);
+vector<bool> visited(n);
+
+void dfs(int current_node) {
+	if (visited[current_node]) { return; }
+	visited[current_node] = true;
+
+	for (int neighbor : adj[current_node]) { dfs(neighbor); }
+}
+
+BFS
+
+for (int i = 0; i < n; i++) {
+		// iterate over all connected components in the graph
+		if (!visited[i]) {
+			queue<int> q;
+			q.push(i);
+			visited[i] = true;
+			while (!q.empty()) {
+				int current_node = q.front();
+				q.pop();
+				for (int neighbor : adj[current_node]) {
+					if (!visited[neighbor]) {
+						visited[neighbor] = true;
+						q.push(neighbor);
+					}
+				}
+			}
+		}
+	}
+
+SIEVE
+
+void sieve(int n,vector<bool> &prime)
+{
+    for(int i=2;i*i<=n;i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int p=i*i;p<=n;p+=i)
+            {
+                prime[p]=false;
+            }
+        }
+    }
+}
+prime[0]=prime[1]=false;
+
+*/
+
+/*void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}*/
+
+void solve()
+{
+    int n,k; cin>>n>>k;
+    vector<char> s(n+1);
+    vector<int> pf_boys(n+1);
+    vector<int> pf_girls(n+1);
+    vector<int> sf_boys(n+1);
+    vector<int> sf_girls(n+1);
+
+    for(int i=1;i<=n;i++)
+    {
+        cin>>s[i];
+    }
+    for(int i=2;i<=n;i++)
+    {
+        if(s[i-1]=='B')
+        {
+            pf_boys[i]=pf_boys[i-1]+1;
+            pf_girls[i]=0;
+        }
+        else
+        {
+            pf_girls[i]=pf_girls[i-1]+1;
+            pf_boys[i]=0;
+        }
+    }
+
+    for(int i=n-1;i>=1;i--)
+    {
+        if(s[i+1]=='B')
+        {
+            sf_boys[i]=sf_boys[i+1]+1;
+            sf_girls[i]=0;
+        }
+        else
+        {
+            sf_girls[i]=sf_girls[i+1]+1;
+            sf_boys[i]=0;
+        }
+    }
+
+    /*for(int i=1;i<=n;i++)
+    {
+        cout << pf_boys[i] << " ";
+    }
+    cout << endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout << pf_girls[i] << " ";
+    }
+    cout << endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout << sf_boys[i] << " ";
+    }
+    cout << endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout << sf_girls[i] << " ";
+    }
+    cout << endl;*/
+
+    bool done = true;
+    int fix = -1;
+
+    vector<int> fixes;
+
+    for(int i=1;i<=n;i++)
+    {
+        int ct = 0; int ptr = i;
+        while(ptr<=n&&s[i]==s[ptr])
+        {
+            ++ct;
+            ++ptr;
+        }
+        if(ct>=k)
+        {
+            if(done)
+            {
+                done = false;
+                fix=i+k-1;
+                fixes.push_back(fix);
+            }
+            else
+            {
+                fixes.push_back(i+k-2);
+            }
+        }
+        i=ptr-1;
+    }
+
+    if(done)
+    {
+        cout << "YES" << endl; return;
+    }
+
+    cout << fixes.size() << endl;
+    
+    if(fixes.size()>2)
+    {
+        cout << "NO" << endl; return;
+    }
+
+    char rep_with = 'B';
+    if(s[fix]=='B') rep_with = 'G';
+
+    cout << rep_with << endl;
+
+    if(fixes.size()==2)
+    {
+        int i = fixes[1];
+        if(s[fixes[0]]==s[fixes[1]])
+        {
+            cout << "NO" << endl; return;
+        }
+        cout << fix << " " << i << endl;
+
+        if(rep_with=='B')
+        {
+            if(sf_girls[i]+1<k&&pf_boys[fix]+1<k)
+            {
+                cout << fix << " " << i << endl;
+                for(int j=fix,h=0;j<=(fix+i)/2;j++,h++)
+                {
+                    swap(s[j],s[i-h]);
+                }
+            }
+        }
+        else
+        {
+            if(sf_boys[i]+1<k&&pf_girls[fix]+1<k)
+            {
+                cout << fix << " " << i << endl;
+                for(int j=fix,h=0;j<=(fix+i)/2;j++,h++)
+                {
+                    swap(s[j],s[i-h]);
+                }
+            }
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            int ct = 0; int ptr = i;
+            while(ptr<=n&&s[i]==s[ptr])
+            {
+                ++ct;
+                ++ptr;
+            }
+            if(ct>=k)
+            {
+                cout << "NO" << endl; return;
+            }
+            i=ptr-1;
+        }
+        cout << "YES" << endl;
+    }
+
+    for(int i=n;i>=fix+1;i++)
+    {
+        if(s[i]==rep_with)
+        {
+            if(rep_with=='B')  // G left, B right
+            {
+                if(sf_girls[i]+1<k&&pf_boys[fix]+1<k)
+                {
+                    cout << fix << " " << i << endl;
+                    for(int j=fix,h=0;j<=(fix+i)/2;j++,h++)
+                    {
+                        swap(s[j],s[i-h]);
+                    }
+                    break;
+                }
+            }
+            else   // B left, G right
+            {
+                if(sf_boys[i]+1<k&&pf_girls[fix]+1<k)
+                {
+                    cout << fix << " " << i << endl;
+                    for(int j=fix,h=0;j<=(fix+i)/2;j++,h++)
+                    {
+                        swap(s[j],s[i-h]);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        cout << s[i];
+    }
+    cout << endl;
+
+    for(int i=1;i<=n;i++)
+    {
+        int ct = 0; int ptr = i;
+        while(ptr<=n&&s[i]==s[ptr])
+        {
+            ++ct;
+            ++ptr;
+        }
+        if(ct>=k)
+        {
+            cout << "NO" << endl; return;
+        }
+        i=ptr-1;
+    }
+    cout << "YES" << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.precision(20);
+
+    //setIO("problemname");
+
+    int t; cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
